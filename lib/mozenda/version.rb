c09a0a3a0e0c43729272b56be54ed79d07dab30d
Jeff Miller <jeffery.f@gmail.com>
2013-06-03 21:52:45 -0500

    Adding basic file infrastructure

---
+++ lib/mozenda/version.rb
@@ -0,0 +1,3 @@
+module Mozenda
+  VERSION = "0.0.1"
+end
---
+++ test/lib/mozenda/version_test.rb
@@ -0,0 +1,9 @@
+require_relative '../../test_helper'
+
+describe Mozenda do
+
+  it "must be defined" do
+    Mozenda::VERSION.wont_be_nil
+  end
+
+end
